bbdce7b1b9759d7770b1cf7e149c18415491153a
Ioan Termure <ioan.termure24@gmail.com>
2023-11-12 13:08:53 +0200

    Implements Reactive Mono find Employee by id request

--- src/main/java/net/javaguides/springboot/service/EmployeeService.java
+++ src/main/java/net/javaguides/springboot/service/EmployeeService.java
@@ -5,4 +5,6 @@ import reactor.core.publisher.Mono;
 
 public interface EmployeeService {
     Mono<EmployeeDto> saveEmployee(EmployeeDto employeeDto);
+    Mono<EmployeeDto> getEmployee(String employeeId);
+
 }
